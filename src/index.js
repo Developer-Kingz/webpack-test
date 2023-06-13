582f43f2e5e43ce8bbdff93a01a009e9790b10d7
Kingsley Igbor <chidoziek2@gmail.com>
2023-06-13 13:40:04 +0100

    setup webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import myImage from "./assets/newimage.jpeg";
+import "../src/style.css";
+
+const title = document.createElement("h3");
+title.textContent = "Webpack test!";
+
+const page = document.querySelector("body");
+page.append(title);
+
+// const img = new Image();
+// img.src = myImage;
+// page.appendChild(img);
